b0843005ce669df76efb84dbd9e54592bc4754f7
Leonardo Pereira Campos <leonardocampos@fiotec.fiocruz.br>
2023-11-13 17:12:18 -0300

    Implementando o Controller, context, migration e services.

--- src/Usuarios/Models/Usuario.cs
+++ src/Usuarios/Models/Usuario.cs
@@ -1,6 +1,10 @@
-﻿namespace Usuarios.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Usuarios.Models
 {
-    public class Usuario
+    public class Usuario : IdentityUser
     {
+        public DateTime DataNascimento { get; set; }
+        public Usuario() : base() { }
     }
 }
